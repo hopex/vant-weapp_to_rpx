d3b8b5c2021da5d27a9fa3b59ee56927d7ef44dd
xinghan <xhopex@qq.com>
2019-10-04 20:06:26 +0800

    init

---
+++ gulpfile.js
@@ -0,0 +1,23 @@
+const { src, dest } = require('gulp');
+const postcss = require('gulp-postcss');
+const pxtorem = require('postcss-pxtransform');
+
+
+exports.wxss = () => {
+	let options={
+		//平台类型
+		platform: 'weapp',
+		//设备宽度 对应iphone 6
+		designWidth: 750,
+		//转换比例 覆盖插件默认比例
+		deviceRatio:{
+			'750':0.5
+		}
+	}
+    let processors = [
+        pxtorem(options)
+    ];
+    return src('src/**/*.wxss')
+        .pipe(postcss(processors))
+        .pipe(dest('dist'));
+};
